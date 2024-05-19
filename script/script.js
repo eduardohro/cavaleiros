587f2aa7334880d5198242ea12dcfbf554bf1c06
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2024-05-19 16:18:45 -0300

    add mission-vision-values

--- script/script.js
+++ script/script.js
@@ -12,29 +12,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const images = backgroundImages.getElementsByClassName('background-image');
     let totalImages = images.length;
     let currentImageIndex = 0;
-    let interval = 3000; // Tempo em milissegundos
+    let interval = 3000; 
 
     function showNextImage() {
-        // Esconde a imagem atual
+        
         images[currentImageIndex].classList.remove('show');
         
-        // Avança para a próxima imagem
+        
         currentImageIndex = (currentImageIndex + 1) % totalImages;
         
-        // Mostra a próxima imagem
+        
         images[currentImageIndex].classList.add('show');
         
-        // Reinicia a animação da barra de progresso
+        
         progressBar.style.width = '0';
-        progressBar.style.animation = 'none'; // Remove a animação
-        void progressBar.offsetWidth; // Força reflow
-        progressBar.style.animation = `fillProgressBar ${interval / 1000}s linear forwards`; // Adiciona a animação novamente
-        progressBar.style.width = '100%'; // Preenche a barra
+        progressBar.style.animation = 'none'; 
+        void progressBar.offsetWidth; 
+        progressBar.style.animation = `fillProgressBar ${interval / 1000}s linear forwards`; 
+        progressBar.style.width = '100%'; 
     }
 
     setInterval(showNextImage, interval);
     
-    // Inicia a animação da barra de progresso ao carregar a página
+    
     progressBar.style.animation = `fillProgressBar ${interval / 1000}s linear forwards`;
     progressBar.style.width = '100%';
 });
